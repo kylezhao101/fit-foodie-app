d344b52e0729a18e6aafedd6c7335a269ad7e777
kylezhao101 <kylezhao101@gmail.com>
2024-02-21 20:52:42 -0800

    Add location permission requesting and handling at runtime

--- app/src/main/java/com/example/fitnessgachaapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessgachaapp/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.fitnessgachaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.Manifest;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Location permission request code
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Call permission request method
+        requestLocationPermission();
+    }
+
+    private void requestLocationPermission() {
+        // Check if permission in manifest is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Not granted, request it
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            // Granted
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Check request code
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission was granted
+                startLocationUpdates();
+            } else {
+                // Permission denied
+            }
+        }
+    }
+
+    private void startLocationUpdates() {
     }
 }
